f77fd82c92cf65ab725f668ac66b5e80d7bcb4aa
Kayo Pereira <kayo.richard2000@gmail.com>
2024-04-29 17:06:17 -0300

    change a price card

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_15_234139) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_29_162315) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_15_234139) do
     t.datetime "day_and_hour"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "photo_link"
   end
 
   create_table "users", force: :cascade do |t|
